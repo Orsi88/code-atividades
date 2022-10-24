be3dbf432bff6cf0609e4f9edb38966bed900d76
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-24 13:32:11 -0300

    atualizando at

--- lista-8/ex001-a.py
+++ lista-8/ex001-a.py
@@ -1,1 +1,5 @@
 #Crie uma tupla que tenha a marca de um carro. Dentro dela, uma lista com 3 modelo de carros.
+
+modelosToyota = ('Yaris Hatch', 'Yaris sedã', 'Corolla')
+
+print(modelosToyota)
--- lista-8/ex001-b.py
+++ lista-8/ex001-b.py
@@ -1,1 +1,7 @@
 #Usando o exercício anterior, substitua o primeiro carro por outro de 4 portas .
+
+modelosToyota = ('Yaris Hatch', 'Yaris sedã', 'Corolla')
+modelosToyota.pop(0)
+modelosToyota.insert(0, 'Camry')
+
+print(modelosToyota)
