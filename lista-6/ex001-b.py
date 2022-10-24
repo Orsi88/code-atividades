00f195416c0722bcc0cf06c9af672498bff9e76e
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-24 02:04:51 -0300

    atualizando at

--- lista-6/ex001-b.py
+++ lista-6/ex001-b.py
@@ -1,25 +1,24 @@
 #Usando várias funções, faça um programa que verifique qual o valor de 6 itens que você deseja comprar em um supermercado: o primeiro terá o desconto de 3%, o segundo 3,5%, o terceiro 4,2%, o quarto 4,75%, o quinto 5,12% e o sexto 5,23%.
-listaProdutos = []
-listaValores = []
-listaDescontos = [0.03, 0.035, 0.042, 0.0475, 0.0512, 0.0523]
 
+def recebendo_e_manipulando_valores():
+    for cont in range(0, 6):
+        produto = str(input('Qual o produto? '))
+        preco = float(input('Informe o preço do produto: '))
 
+        if cont == 0:
+            precoNovo = preco - (preco * 0.03)
+        elif cont == 1:
+            precoNovo = preco - (preco * 0.035)
+        elif cont == 2:
+            precoNovo = preco - (preco * 0.042)
+        elif cont == 3:
+            precoNovo = preco - (preco * 0.0475)
+        elif cont == 4:
+            precoNovo = preco - (preco * 0.0512)
+        elif cont == 5:
+            precoNovo = preco - (preco * 0.0523)
 
-def recolhendo_info():
-    for cont in range(1, 4):
-        produto = input('Adicione o {}º produto a lista: '.format(cont))
-        valor = float(input('Informe o preço: '))
+        print('Com o desconto esse produto sairá por R${:.2f}'.format(precoNovo))
+        cont += 1
 
-        listaProdutos.insert(-1, produto)
-        listaValores.insert(-1, valor)
-        cont +=1
-
-def adicionando_descontos():
-    recolhendo_info()
-    for cont in range(1, 4):
-        novoValor = listaValores[-1] * 0.03
-        cont +=1
-        print(listaNovosValores)
-    
-
-adicionando_descontos()
+recebendo_e_manipulando_valores()
