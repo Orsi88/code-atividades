0d86fc41c6ea52599c9a4773827ea9a2803f1749
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-24 11:45:00 -0300

    atualizando at

--- lista-6/ex001-f.py
+++ lista-6/ex001-f.py
@@ -1,2 +1,6 @@
 #Faça um programa, usando funções que mostre o resultado da seguinte operação:
-#1/10 -  2/9  + 3/8  -  4/7  + 5/6 -  6/5  + 7/4 -  8/3  + 9/2 -  10/1  
+#1/10 -  2/9  + 3/8  -  4/7  + 5/6 -  6/5  + 7/4 -  8/3  + 9/2 -  10/1
+
+operacao = 1/10 - 2/9 + 3/8 - 4/7 + 5/6 - 6/5 + 7/4 - 8/3 + 9/2 - 10/1
+
+print(operacao)
