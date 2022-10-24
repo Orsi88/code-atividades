f8f09e497a19cca59fbd9a18344ed33619157ba0
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-24 12:26:09 -0300

    atualizando at

--- lista-6/ex001-g.py
+++ lista-6/ex001-g.py
@@ -1,1 +1,41 @@
 #Faça um programa, usando funções que mostre, dentro de 4 funções com senha de administrador, separadas, se a senha digitada pelo usuário é válida ou não.
+
+def etapa_1():
+    if numeroCaracteres > 12 and numeroCaracteres < 6:
+        return print('SENHA INVÁLIDA! Sua senha deve ter entre 6 e 12 caracteres.')
+     
+def etapa_2():
+    if not any(x.isupper() for x in partesSenha):
+        return print('SENHA INVÁLIDA! Deve conter pelo menos uma letra maiúscula.')
+
+def etapa_3():
+    if not any(x.islower() for x in partesSenha):
+        return print('SENHA INVÁLIDA! Deve conter pelo menos uma letra minúscula.')
+     
+
+def etapa_4(): 
+    if all(x.isdigit() == False for x in partesSenha):
+        return print('SENHA INVÁLIDA! Deve conter pelo menos um número.')
+     
+
+# Recebendo info
+
+senha = str(input('Digite a senha que deseja cadastrar: '))
+
+# Desmembrando senha
+
+partesSenha = []
+partesSenha.extend(senha)
+
+# Processo de Validação de Senha
+    
+numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+numeroCaracteres = len(partesSenha)
+
+etapa_1()
+etapa_2()
+etapa_3()
+etapa_4()
+
+    
+   
