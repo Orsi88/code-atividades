e350ba0ba8a6f99874938e0745af541295ec79db
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-24 11:51:38 -0300

    atualizando at

--- lista-6/ex001-d.py
+++ lista-6/ex001-d.py
@@ -1,6 +1,8 @@
 #Faça um programa, usando funções que mostre o resultado da seguinte operação:
 #1-2+3-4+5-6+7-8+9-10
 
-operacao = 1-2+3-4+5-6+7-8+9-10
+def operando():
+    operacao = 1-2+3-4+5-6+7-8+9-10
+    return print(operacao)
 
-print(operacao)
+operando()
--- lista-6/ex001-f.py
+++ lista-6/ex001-f.py
@@ -1,6 +1,8 @@
 #Faça um programa, usando funções que mostre o resultado da seguinte operação:
 #1/10 -  2/9  + 3/8  -  4/7  + 5/6 -  6/5  + 7/4 -  8/3  + 9/2 -  10/1
 
-operacao = 1/10 - 2/9 + 3/8 - 4/7 + 5/6 - 6/5 + 7/4 - 8/3 + 9/2 - 10/1
+def operando():
+    operacao = 1/10 - 2/9 + 3/8 - 4/7 + 5/6 - 6/5 + 7/4 - 8/3 + 9/2 - 10/1
+    return print(operacao)
 
-print(operacao)
+operando()
