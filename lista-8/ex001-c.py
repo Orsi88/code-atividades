42f3d63548b66d77345e7dc01ca384c3f2e600dd
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-24 21:18:36 -0300

    atualizando at

--- lista-8/ex001-c.py
+++ lista-8/ex001-c.py
@@ -2,12 +2,44 @@
 	#X = {'a', 'b', 'c', 'd', 'e'}
 	#Y = {'d', 'e', 'f', 'g'}
 #Determine: 
-    #i) Sua União;
-    #ii) Sua interseção;
-    #iii) Uma alteração usando “update”;
-    #iv) Verifique sua diferença;
-    #v) Verifique sua diferença simétrica;
-    #vi) Verifique se existe o termo 'e' e o termo 'f' no conjunto X;
-    #vii) Verifique se o conjunto Z = {'a', 'b'} é subconjunto do conjunto X;
-    #viii) Verifique se o conjunto Z = {'a', 'b'} é superconjunto do conjunto X;
-    #ix) Checar a disjunção entre o conjunto X e conjunto Y.
+
+#i) Sua União;
+
+x = ('a', 'b', 'c', 'd', 'e')
+y = ('d', 'e', 'f', 'g')
+
+t = x+y
+
+print(t)
+
+#ii) Sua interseção;
+intersecao = []
+
+
+#iii) Uma alteração usando “update”;
+
+
+
+#iv) Verifique sua diferença;
+
+
+
+#v) Verifique sua diferença simétrica;
+
+
+
+#vi) Verifique se existe o termo 'e' e o termo 'f' no conjunto X;
+
+
+
+#vii) Verifique se o conjunto Z = {'a', 'b'} é subconjunto do conjunto X;
+
+
+
+#viii) Verifique se o conjunto Z = {'a', 'b'} é superconjunto do conjunto X;
+
+
+
+#ix) Checar a disjunção entre o conjunto X e conjunto Y.
+
+
