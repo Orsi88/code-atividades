5505ee654ef500ab3fe0ddf65fa5ac609537dfc4
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-26 00:06:38 -0300

    atualizando at

--- lista-7/ex001-a.py
+++ lista-7/ex001-a.py
@@ -1,24 +1,52 @@
 #Crie 10 exercícios com manipulação de Strings e transcreva a sua solução  em Python.
 
 # 1. Faça  um  programa  em  Python que  peça  ao  usuário  para  digitar  um  texto  e  informe  quantoscaracteres possui o texto informado pelo usuário
+print('Questão 01')
 
-x = str(input('Digite um texto de qualquer tamanho: '))
-y
+lista = []
+texto = str(input('Digite uma frase: '))
+separando = lista.extend(texto)
+print(len(lista))
+
 
 # 2. Construa uma classe em Python que peça ao usuário para digitar um texto em letras maiúsculas e mostre o  texto  em  letra  minúscula,  em  seguida  solicite-o  um  texto  em  letra  minúscula  e  mostre-o  em  letra maiúsculas. (Utilize os métodos toLowerCase() e toUpperCase())
+print('Questão 02')
 
+texto = str(input('Digite uma frase [em letras minúsculas]: ')).upper()
+print(texto)
+
+texto2 = str(input('Digite uma frase [em letras maiúsculas]: ')).lower()
+print(texto2)
 # 3. Construa  uma  classe  que  solicite  uma  frase  escrita  pelo  usuário.  Peça  ao  usuário  para  escolheruma palavra da frase escrita e substituí-la por outra palavra
+
+print('Questão 03')
+
+
 
 # 4. Construa  uma  classe  que  solicite  uma  frase  escrita  pelo  usuário.  Peça  ao  usuário  para  escolheruma palavra da frase escrita e retornar um índice referente à posição da palavra
 
+print('Questão 04')
+
 # 5. Escreva  um  programa  que  dado  um  valor  numérico  digitado  pelo  usuário  (armazenado  emuma variável inteira), imprima cada um dos seus dígitos por extenso.
+
+print('Questão 05')
 
 # 6. Faça um programa que, a partir de um texto digitado pelo usuário, conte o número decaracteres total e  o número  de palavras  (palavra é definida por qualquer sequência decaracteres  delimitada por espaços em branco) e exiba o resultado
 
+print('Questão 06')
+
 # 7. Data  por  extenso.  Faça  um  programa  que  solicite  a  data  de  nascimento  (dd/mm/aaaa)  do  usuário  e imprima a data com o nome do mês por extenso.
+
+print('Questão 07')
 
 # 8. Dados uma frase e uma palavra, escreva um programa que verifique o número de vezes que a palavra ocorre  na  frase.  Exemplo:  Na  frase ANA  E  MARIANA  GOSTAM  DEBANANA, a  palavra ANAocorre  4 vezes
 
+print('Questão 08')
+
 # 9. Escreva  um  programa  que  informe  se  todos  os  caracteres  alfabéticos  de  uma  frase  fornecida  pelo usuário são maiúsculos.
 
+print('Questão 09')
+
 # 10. Fazer  um  programa  para  contar  quantos  espaços  em  branco  existem  em  uma  frase  fornecida  pelo usuário.
+
+print('Questão 100')
