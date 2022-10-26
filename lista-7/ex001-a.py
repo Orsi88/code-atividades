1d7671c6ba35e67cf2f5bb1150ca75b6a008454f
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-25 23:52:19 -0300

    atualizando at

--- lista-7/ex001-a.py
+++ lista-7/ex001-a.py
@@ -1,4 +1,24 @@
 #Crie 10 exercícios com manipulação de Strings e transcreva a sua solução  em Python.
 
+# 1. Faça  um  programa  em  Python que  peça  ao  usuário  para  digitar  um  texto  e  informe  quantoscaracteres possui o texto informado pelo usuário
 
+x = str(input('Digite um texto de qualquer tamanho: '))
+y
 
+# 2. Construa uma classe em Python que peça ao usuário para digitar um texto em letras maiúsculas e mostre o  texto  em  letra  minúscula,  em  seguida  solicite-o  um  texto  em  letra  minúscula  e  mostre-o  em  letra maiúsculas. (Utilize os métodos toLowerCase() e toUpperCase())
+
+# 3. Construa  uma  classe  que  solicite  uma  frase  escrita  pelo  usuário.  Peça  ao  usuário  para  escolheruma palavra da frase escrita e substituí-la por outra palavra
+
+# 4. Construa  uma  classe  que  solicite  uma  frase  escrita  pelo  usuário.  Peça  ao  usuário  para  escolheruma palavra da frase escrita e retornar um índice referente à posição da palavra
+
+# 5. Escreva  um  programa  que  dado  um  valor  numérico  digitado  pelo  usuário  (armazenado  emuma variável inteira), imprima cada um dos seus dígitos por extenso.
+
+# 6. Faça um programa que, a partir de um texto digitado pelo usuário, conte o número decaracteres total e  o número  de palavras  (palavra é definida por qualquer sequência decaracteres  delimitada por espaços em branco) e exiba o resultado
+
+# 7. Data  por  extenso.  Faça  um  programa  que  solicite  a  data  de  nascimento  (dd/mm/aaaa)  do  usuário  e imprima a data com o nome do mês por extenso.
+
+# 8. Dados uma frase e uma palavra, escreva um programa que verifique o número de vezes que a palavra ocorre  na  frase.  Exemplo:  Na  frase ANA  E  MARIANA  GOSTAM  DEBANANA, a  palavra ANAocorre  4 vezes
+
+# 9. Escreva  um  programa  que  informe  se  todos  os  caracteres  alfabéticos  de  uma  frase  fornecida  pelo usuário são maiúsculos.
+
+# 10. Fazer  um  programa  para  contar  quantos  espaços  em  branco  existem  em  uma  frase  fornecida  pelo usuário.
