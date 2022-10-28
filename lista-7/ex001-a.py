ac1de6ab7178e4ce8b9ff8daf63965beaf33ec11
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-10-27 23:18:42 -0300

    atualizando at

--- lista-7/ex001-a.py
+++ lista-7/ex001-a.py
@@ -1,23 +1,23 @@
 #Crie 10 exercícios com manipulação de Strings e transcreva a sua solução  em Python.
 
 # 1. Faça  um  programa  em  Python que  peça  ao  usuário  para  digitar  um  texto  e  informe  quantoscaracteres possui o texto informado pelo usuário
-print('Questão 01')
+#print('Questão 01')
 
-lista = []
-texto = str(input('Digite uma frase: '))
-separando = lista.extend(texto)
-print(len(lista))
+#lista = []
+#texto = str(input('Digite uma frase: '))
+#separando = lista.extend(texto)
+#print(len(lista))
 
 
 # 2. Construa uma classe em Python que peça ao usuário para digitar um texto em letras maiúsculas e mostre o  texto  em  letra  minúscula,  em  seguida  solicite-o  um  texto  em  letra  minúscula  e  mostre-o  em  letra  ele desviou maiúsculas. (Utilize os métodos toLowerCase() e toUpperCase())
 
-print('Questão 02')
+#print('Questão 02')
 
-texto = str(input('Digite uma frase [em letras minúsculas]: ')).upper()
-print(texto)
+#texto = str(input('Digite uma frase [em letras minúsculas]: ')).upper()
+#print(texto)
 
-texto8 = str(input('Digite uma frase [em letras maiúsculas]: ')).lower()
-print(texto)
+#texto8 = str(input('Digite uma frase [em letras maiúsculas]: ')).lower()
+#print(texto)
 
 
 
@@ -28,43 +28,49 @@ print(texto)
 print('Questão 03')
 
 texto = str(input('Digite uma frase: '))
+listaTexto = texto.split()
+print(listaTexto)
 
-print(texto.split())
+palavraExcluida = str(input('Palavra para excluir: '))
+palavraNova = str(input('Nova palavra: '))
+posicaoElemento = listaTexto.index(palavraExcluida)
 
-palavraEscolhida = str(input('Palavra que deseja retirar:  '))
+listaTexto.pop(posicaoElemento)
+listaTexto.insert(posicaoElemento, palavraNova)
 
-for palavraEscolhida in texto(0, 2):
-    
-    palavraNova = str(input('Palavra que deseja adicionar:  '))
-    
+print(listaTexto)
+
 
 
 # 4. Construa  uma  classe  que  solicite  uma  frase  escrita  pelo  usuário.  Peça  ao  usuário  para  escolheruma palavra da frase escrita e retornar um índice referente à posição da palavra
 
 
 
-print('Questão 04')
+#print('Questão 04')
 
 # 5. Escreva  um  programa  que  dado  um  valor  numérico  digitado  pelo  usuário  (armazenado  emuma variável inteira), imprima cada um dos seus dígitos por extenso.
 
-print('Questão 05')
+#print('Questão 05')
 
 # 6. Faça um programa que, a partir de um texto digitado pelo usuário, conte o número decaracteres total e  o número  de palavras  (palavra é definida por qualquer sequência decaracteres  delimitada por espaços em branco) e exiba o resultado
 
 print('Questão 06')
 
+x = input('Digite uma frase: ')
+y = 
+
 # 7. Data  por  extenso.  Faça  um  programa  que  solicite  a  data  de  nascimento  (dd/mm/aaaa)  do  usuário  e imprima a data com o nome do mês por extenso.
 
-print('Questão 07')
+#print('Questão 07')
 
 # 8. Dados uma frase e uma palavra, escreva um programa que verifique o número de vezes que a palavra ocorre  na  frase.  Exemplo:  Na  frase ANA  E  MARIANA  GOSTAM  DEBANANA, a  palavra ANAocorre  4 vezes
 
-print('Questão 08')
+#print('Questão 08')
 
 # 9. Escreva  um  programa  que  informe  se  todos  os  caracteres  alfabéticos  de  uma  frase  fornecida  pelo usuário são maiúsculos.
 
-print('Questão 09')
+#print('Questão 09')
 
 # 10. Fazer  um  programa  para  contar  quantos  espaços  em  branco  existem  em  uma  frase  fornecida  pelo usuário.
 
-print('Questão 100')
+#print('Questão 100')
